8e6d7a18951c7096e70f4c505eaefb165fd06de7
Dennis Walters <pooster@gmail.com>
2019-01-06 04:57:56 -0500

    Added tests

--- aescrypter.go
+++ aescrypter.go
@@ -1,21 +1,29 @@
+// Package aescrypter provides a sekrat.Crypter implementation that uses the
+// AES-256-GCM algorithm to encrypt/decrypt data.
 package aescrypter
 
 import (
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha256"
 	"errors"
 	"io"
+
+	"github.com/sekrat/sekrat"
 )
 
 const (
 	nonceSize = 16
 )
 
+// Crypter is a sekrat.Crypter implementation.
 type Crypter struct{}
 
+// Encrypt takes a key and a chunk of data, encrypts it, and returns an
+// encrypted payload (as a byte array) and an error. If there are any issues
+// along the way, the payload is nil and the error is populated. Otherwise,
+// the payload is populated and the error is nil.
 func (crypter *Crypter) Encrypt(key string, data []byte) ([]byte, error) {
 	hashKey := normalize(key)
 
@@ -39,6 +47,10 @@ func (crypter *Crypter) Encrypt(key string, data []byte) ([]byte, error) {
 	return ciphertext, nil
 }
 
+// Decrypt takes an encryption key and an encrypted payload, decrypts the
+// payload, and returns the decrypted data and an error. If there are any
+// issues along the way, the data is nil and the error is populated. Otherwise,
+// the data is populated and the error is nil.
 func (crypter *Crypter) Decrypt(key string, data []byte) ([]byte, error) {
 	hashKey := normalize(key)
 
@@ -73,7 +85,8 @@ func normalize(key string) []byte {
 	return sum
 }
 
-func New() *Crypter {
+// New creates a new instance of Crypter and returns it.
+func New() sekrat.Crypter {
 	return &Crypter{}
 }
 
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/sekrat/aescrypter
+
+require github.com/sekrat/sekrat v1.0.1
